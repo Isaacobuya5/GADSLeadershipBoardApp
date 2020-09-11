a14624e5980b71b2070cdbf366e2beeb93d69a5a
Isaac Obuya <Szack4477@gmail.com>
2020-09-11 21:13:29 +0300

    fix issues with confirm modal windows

--- app/src/main/java/com/example/gadsleadershipboardapp/MainActivity.java
+++ app/src/main/java/com/example/gadsleadershipboardapp/MainActivity.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int DELAY_HOME_ACTIVITY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LeaderBoardActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, LeaderBoardActivity.class);
+//        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent firstActivity = new Intent(MainActivity.this, LeaderBoardActivity.class);
+                startActivity(firstActivity);
+                finish();
+            }
+        }, DELAY_HOME_ACTIVITY);
+
     }
 }
